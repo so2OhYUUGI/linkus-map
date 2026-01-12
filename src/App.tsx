90eaa3a6918f7634210e1256df08f6108e69c2c7
so2 <so2jin5@gmail.com>
2026-01-12 15:02:54 +0900

    微調整

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       }}
     >
       <Typography variant="h1">
-        linkus-map
+        Linkus-MAP
       </Typography>
     </Box>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme/theme';
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
